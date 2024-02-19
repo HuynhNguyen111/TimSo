f04868d6a95cc776fef8cbb26d895dc061a69ae5
Nguyễn Thế Phương Huynh <huynhnguyen2032001@gmail.com>
2024-02-19 11:28:56 +0700

    1st commit

---
+++ 7-TimSo/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		for (int i = 10; i <= 200; i++) {
+			if (i % 7 == 0 && i % 5 != 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
